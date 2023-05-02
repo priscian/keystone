351657358a4e34445ae72c0161e51329fc0cd2d2
priscian <priscian@gmail.com>
2023-05-02 13:51:39 -0400

    Added 'LOESS()' function.

--- R/stats.R
+++ R/stats.R
@@ -27,3 +27,104 @@ bd_rsd <- function(x, na.rm = FALSE, ...) stats::median(abs(x - stats::median(x,
 ## BD Biosciences robust coefficient of variation (BD-rCV)
 #' @export
 bd_rcv <- function(x, na.rm = FALSE, ...) bd_rsd(x, na.rm = na.rm) / stats::median(x, na.rm = na.rm)
+
+
+## Adapted from 'fANCOVA::loess.as()'; automatically choose 'span' parameter for 'stats::loess()'
+optimize_span <- function(
+  model,
+  criterion = c("aicc", "gcv"),
+  span_range = c(0.05, 0.95)
+)
+{
+  as.crit <- function(x)
+  {
+    span <- x$pars$span
+    traceL <- x$trace.hat
+    sigma2 <- sum(x$residuals^2)/(x$n - 1)
+    aicc <- log(sigma2) + 1 + 2 * (2 * (traceL + 1))/(x$n - traceL - 2)
+    gcv <- x$n * sigma2/(x$n - traceL)^2
+    result <- list(span = span, aicc = aicc, gcv = gcv)
+
+    return(result)
+  }
+
+  criterion <- match.arg(criterion)
+  fn <- function(span)
+  {
+    mod <- update(model, span = span)
+
+    as.crit(mod)[[criterion]]
+  }
+
+  result <- stats::optimize(fn, span_range)
+
+  #return (list(span = result$minimum, criterion = result$objective))
+  return (result$minimum)
+}
+
+
+## Drop-in replacement for 'stats::loess()' that chooses optimal span/bandwidth parameter when 'span = NULL';
+##   also produces a quick-view viz for 'plot = TRUE'.
+#' @export
+LOESS <- function(
+  formula,
+  data,
+  span = formals(stats::loess)$span,
+  plot = FALSE,
+  optimize_span... = list(),
+  ...
+)
+{
+  opt.span <- optimize_span %>% `environment<-`(environment()) # Otherwise 'stats::optimize()' fails
+  form <- formula %>% `environment<-`(environment()) # Allows use of e.g. 'stats::model.frame()'
+
+  if (is.null(span)) {
+    optimize_spanArgs <- list(
+      model = stats::loess(formula = form, data = data, ...)
+    )
+    optimize_spanArgs <- utils::modifyList(optimize_spanArgs, optimize_span..., keep.null = TRUE)
+
+    span <- do.call(opt.span, optimize_spanArgs)
+  }
+
+  fit <- stats::loess(formula = form, data = data, span = span, ...)
+
+  if (plot) { ## Adapted from 'fANCOVA::loess.as()'
+    x <- fit$x
+    fitPlot <- update(fit, control = loess.control(surface = "direct"))
+
+    if (NCOL(x) == 1) {
+      m <- 100
+      xNew <- seq(min(x, na.rm = TRUE), max(x, na.rm = TRUE), length.out = m)
+      fitNew <- stats::predict(fitPlot, dataframe(xNew) %>% `names<-`(tail(all.vars(form), -1)))
+
+      plot(form, data, col = "grey", xlab = "x", ylab = "m(x)", ...)
+      lines(xNew, fitNew, lwd = 1.5, ...)
+    } else {
+      m <- 50
+      x1 <- seq(min(x[, 1]), max(x[, 1]), len = m)
+      x2 <- seq(min(x[, 2]), max(x[, 2]), len = m)
+      formVars <- all.vars(form)
+      xNew <- expand.grid(x1 = x1, x2 = x2) %>%
+        `names<-`(tail(formVars, -1)) %>% `length<-`(2)
+      fitNew <- matrix(stats::predict(fitPlot, xNew), m, m)
+
+      graphics::persp(x1, x2, fitNew, theta = 40, phi = 30, ticktype = "detailed",
+        xlab = formVars[2], ylab = formVars[3], zlab = formVars[1], col = "lightblue", expand = 0.6)
+    }
+  }
+
+  return (fit)
+}
+
+## usage:
+# (cars.lo <- LOESS(dist ~ speed, cars, span = NULL, plot = TRUE))
+# n2 <- 100
+# x21 <- runif(n2, min = 0, max = 3)
+# x22 <- runif(n2, min = 0, max = 3)
+# sd2 <- 0.25
+# e2 <- rnorm(n2, sd = sd2)
+# y2 <- sin(2 * x21) + sin(2 * x22) + 1 + e2
+# # (y2.fit <- fANCOVA::loess.as(cbind(x21, x22), y2, plot = TRUE))
+# dat <- cbind(x21, x22, y2) %>% as.data.frame
+# (y2.fit <- LOESS(y2 ~ x21 + x22, dat, span = NULL, plot = TRUE))
